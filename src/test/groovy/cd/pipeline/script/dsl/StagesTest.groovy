184cd58224caffb4c6c7c8d1446d53bfe008da6f
Patrick van Dissel <pvdissel@gmail.com>
2015-11-08 20:52:30 +0100

    Add basic multistage test

--- src/test/groovy/cd/pipeline/script/dsl/StagesTest.groovy
+++ src/test/groovy/cd/pipeline/script/dsl/StagesTest.groovy
@@ -2,6 +2,7 @@ package cd.pipeline.script.dsl
 
 import cd.pipeline.script.DslRunner
 import cd.pipeline.script.ScriptLoader
+import cd.pipeline.script.dsl.job.Action
 import cd.pipeline.script.dsl.job.ArtifactAction
 import cd.pipeline.script.dsl.job.ExecAction
 import spock.lang.Specification
@@ -9,36 +10,53 @@ import spock.lang.Specification
 class StagesTest extends Specification
     implements DslRunner, ScriptLoader {
 
-    def 'Action addition order is kept per Job of a Stage '() {
+    def 'Action addition order is kept per Job of a Stage'() {
         def script = load('SingleStage.groovy')
 
-        expect:
+        when:
         def result = runScript(script)
+
+        then:
         result.script.stages.size() == 1
         StageSpec stage = result.script.stages.first()
         stage.jobs.size() == 1
 
         JobSpec job = stage.jobs.first()
         job.actions.size() == 4
-        def action0 = job.actions.get(0)
-        action0 instanceof ExecAction
-        action0.command == './gradlew clean build'
 
-        def action1 = job.actions.get(1)
-        action1 instanceof ArtifactAction
-        action1.id == 'something'
-        // TODO: find way to check action1.content
-
-        def action2 = job.actions.get(2)
-        action2 instanceof ArtifactAction
-        action2.id == 'jars'
-        // TODO: find way to check action2.content
-
-        def action3 = job.actions.get(3)
-        action3 instanceof ArtifactAction
-        action3.id == 'testReports'
-        // TODO: find way to check action3.content
+        verifyExecAction(job.actions.get(0), './gradlew clean build')
+        verifyArtifactAction(job.actions.get(1), 'something')
+        verifyArtifactAction(job.actions.get(2), 'jars')
+        verifyArtifactAction(job.actions.get(3), 'testReports')
 
         result.output == ''
+    }
+
+    def 'Stage addition order is kept'() {
+        def script = load('MultiStage.groovy')
+
+        when:
+        def result = runScript(script)
+
+        then:
+        result.script.stages.size() == 3
+
+        def stages = result.script.stages.iterator()
+        stages.next().id == 'stage1'
+        stages.next().id == 'stage2'
+        stages.next().id == 'stage3'
+
+        result.output == ''
+    }
+
+    private void verifyExecAction(Action action, String command) {
+        assert action instanceof ExecAction
+        assert action.command == command
+    }
+
+    private void verifyArtifactAction(Action action, String id) {
+        assert action instanceof ArtifactAction
+        assert action.id == id
+        // TODO: find way to check action.content
     }
 }
---
+++ src/test/resources/cd/pipeline/script/dsl/MultiStage.groovy
@@ -0,0 +1,35 @@
+stage('stage1') {
+    job {
+        exec './gradlew clean build'
+
+        // Archive artifacts for use in other stages
+        artifact id: 'something1', content: text('lala')
+        artifact id: 'jars1', content: files('build/lib/*.jar')
+        // or to be able to use the artifacts (eg. test reports/data) outside the pipeline, eg. in UI
+        artifact id: 'testReports1', content: files('build/reports', 'build/test-results/**/*.xml')
+    }
+}
+
+stage('stage2') {
+    job {
+        exec './gradlew clean build'
+
+        // Archive artifacts for use in other stages
+        artifact id: 'something2', content: text('lala')
+        artifact id: 'jars2', content: files('build/lib/*.jar')
+        // or to be able to use the artifacts (eg. test reports/data) outside the pipeline, eg. in UI
+        artifact id: 'testReports2', content: files('build/reports', 'build/test-results/**/*.xml')
+    }
+}
+
+stage('stage3') {
+    job {
+        exec './gradlew clean build'
+
+        // Archive artifacts for use in other stages
+        artifact id: 'something3', content: text('lala')
+        artifact id: 'jars3', content: files('build/lib/*.jar')
+        // or to be able to use the artifacts (eg. test reports/data) outside the pipeline, eg. in UI
+        artifact id: 'testReports3', content: files('build/reports', 'build/test-results/**/*.xml')
+    }
+}
